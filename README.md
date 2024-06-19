668932bc78f42d9af4cdbf355adca565e801fbf3
Jessica Palacios <jpyamakawa@yahoo.com>
2024-06-19 01:41:13 -0400

    finishing lab 5b

--- README.md
+++ README.md
@@ -1,22 +1,13 @@
-# Lab 02
-
+# Lab 02-05
 ## About me
-
 Contruí una página web “About me”, con un cuestionario para el usuario sobre datos curiosos y dándole información acerca de mi.
-
 Luego le hago preguntas al usuario de tipo sí o no.
-
 ### Autor: Jessica Palacios Yamakawa
-
 ### Enlaces y Recursos
-
 * [Guia para Readme](https://entertechschool.github.io/code-201-guide/curriculum/class-02/README-template.html)
 * [Instrucciones del proyecto](https://entertechschool.github.io/code-201-guide/curriculum/class-02/project-setup)
-
 ### Puntuación de Accesibilidad de Lighthouse
-
-* ![Puntuación de accesibilidad Lighthouse](./img/lighthouse-lab02.jpg)
-
+* ![Puntuación de accesibilidad Lighthouse en Laboratorio #2](./img/lighthouse-lab02.jpg)
+* ![Puntuación de accesibilidad Lighthouse en Laboratorio #2](./img/lighthouse-lab05.jpg)
 ### Reflexiones y Comentarios
-
 * Este laboratorio ha sido muy interesante y divertido.
--- js/app.js
+++ js/app.js
@@ -1,107 +1,78 @@
 "use strict";
-
 let nombre = prompt("Hola! Cuál es tu nombre?");
 let counter = 1;
 let puntos = 0;
 let respuestasCorrectas = "";
+let condition = false;
 
 alert("Hola " + nombre + "! Por favor contesta las siguientes preguntas:");
-// Option 1:
-//Aplicar Funciones.
 const respuestaAfirmativa = "Si";
 const respuestaNegativa = "No";
-
-
-
-
-//1 Tengo segundo nombre?
-//const respuesta1 = "No";
-//2 Tengo un trabajo
-//const respuesta2 = "Si";
-//3 Tengo una Maestria
-//const respuesta3 = "Si";
-//4 Tengo un PHD
-//const respuesta4 = "No";
-//5 Trabajo en Google
-//const respuesta5 = "No";
-
-//1 case sensitive
-
-
-//Reducir a Funciones.
-
 function segundoNombre() {
-    const pregunta1 = prompt("Tengo un segundo nombre?");
+    const pregunta1 = prompt("Tengo segundo nombre?");
     if (respuestaNegativa.toLowerCase() === pregunta1.toLowerCase()) {
         alert("Muy bien!");
-        //console.log("Muy bien!");
         puntos++;
     }
     else {
         alert("Respuesta incorrecta!");
-        //console.log("Respuesta incorrecta!");
     }
-
 }
-
 segundoNombre();
 
+function tengoTrabajo() {
+    const pregunta2 = prompt("Tengo un trabajo actualmente?");
+    if (respuestaAfirmativa.toLowerCase() === pregunta2.toLocaleLowerCase()) {
+        alert("Muy bien!");
+        puntos++;
+    }
+    else {
+        alert("Respuesta incorrecta!");
+    }
+}
+tengoTrabajo();
 
-const pregunta2 = prompt("Tengo un trabajo actualmente?");
-if (respuestaAfirmativa.toLowerCase() === pregunta2.toLocaleLowerCase()) {
-    alert("Muy bien!");
-    //console.log("Muy bien!");
-    puntos++;
+function tengoMaestria() {
+    const pregunta3 = prompt("Tengo una Maestria?");
+    if (respuestaAfirmativa.toLowerCase() === pregunta3.toLocaleLowerCase()) {
+        alert("Muy bien!");
+        puntos++;
+    }
+    else {
+        alert("Respuesta incorrecta!");
+    }
 }
-else {
-    alert("Respuesta incorrecta!");
-    //console.log("Respuesta incorrecta!");
-}
+tengoMaestria();
 
-const pregunta3 = prompt("Tengo una Maestria?");
-if (respuestaAfirmativa.toLowerCase() === pregunta3.toLocaleLowerCase()) {
-    alert("Muy bien!");
-    //console.log("Muy bien!");
-    puntos++;
+function tengoPhd() {
+    const pregunta4 = prompt("Tengo un PHD?");
+    if (respuestaNegativa.toLowerCase() === pregunta4.toLocaleLowerCase()) {
+        alert("Muy bien!");
+        puntos++;
+    }
+    else {
+        alert("Respuesta incorrecta!");
+    }
 }
-else {
-    alert("Respuesta incorrecta!");
-    //console.log("Respuesta incorrecta!");
-}
+tengoPhd();
 
-const pregunta4 = prompt("Tengo un PHD?");
-if (respuestaNegativa.toLowerCase() === pregunta4.toLocaleLowerCase()) {
-    alert("Muy bien!");
-    //console.log("Muy bien!");
-    puntos++;
+function trabajoenGoogle() {
+    const pregunta5 = prompt("Trabajo en Google?");
+    if (respuestaNegativa.toLowerCase() === pregunta5.toLocaleLowerCase()) {
+        alert("Muy bien!");
+        puntos++;
+    }
+    else {
+        alert("Respuesta incorrecta!");
+    }
 }
-else {
-    alert("Respuesta incorrecta!");
-    //console.log("Respuesta incorrecta!");
-}
-
-const pregunta5 = prompt("Trabajo en Google?");
-if (respuestaNegativa.toLowerCase() === pregunta5.toLocaleLowerCase()) {
-    alert("Muy bien!");
-    //console.log("Muy bien!");
-    puntos++;
-}
-else {
-    alert("Respuesta incorrecta!");
-    //console.log("Respuesta incorrecta!");
-}
-
-
-
+trabajoenGoogle();
 
 
 const maxIntentos = 4;
 //6 En qué año terminé mi licenciatura
 const respuesta6 = 2010;
-
-
 while (counter <= maxIntentos) {
-
     const pregunta6 = Number(prompt("Adivina en qué año terminé mi licenciatura? Tienes 4 intentos"));
     if (respuesta6 === pregunta6) {
         alert("Respuesta correcta!");
@@ -123,64 +94,25 @@ while (counter <= maxIntentos) {
         }
     }
 }
-
 //7 Menciona un país en el que you haya vivido
 const respuesta7 = ["Peru", "Japon", "Francia", "Alemania", "USA"];
-
 for (let i = 0; i < 6; i++) {
     let pregunta7 = prompt("Menciona uno de los países en los que he vivido. Tienes 6 intentos!");
-    for (let j = 0; j < respuesta7.length; i++) {
+    puntos++;
+    for (let j = 0; j < respuesta7.length; j++) {
         if (pregunta7.toLowerCase() === respuesta7[j].toLowerCase()) {
             alert("Respuesta correcta!");
-            puntos++;
-            alert("Tuviste " + puntos + " respuesta(s) correcta(s)!");
+            condition = true;
             break;
         }
     }
-    if (i === 5) {
-        alert("Se acabaron tus intentos. las respuesta correctas son " + respuesta7[0] + ' ' + respuesta7[1]);
+    if (condition) {
+        alert("Buen trabajo! Tienes en total " + puntos + " puntos");
+        break;
+    }
+    else if (puntos === 5) {
+        alert("Se acabaron tus intentos.");
     } else {
         alert("Sigue intentando");
     }
 }
-
-/*
-for (let j = 0; j < 5; j++) {
-    respuestasCorrectas += respuesta7[j];
-    if (j < 4) {
-        respuestasCorrectas += ", ";
-    }
-}
-
-alert("Tuviste "+ puntos + " respuesta(s) correcta(s). Las respuestas correctas eran: " 
-    + respuestasCorrectas);
-
-
-// puntos += 1;
-//preguntas 6 y 7 del Lab03
-
-//Option 2
-/*const arregloTotal = [
-    ["Yamakawa", "Cual es mi segundo apellido?"], //primer arreglo y Anderson es 
-    //la respuesta correcta y a la vez es el primer arreglo del primer arreglo
-    ["YBUSA", "En que empresa trabajo actualment?"],//segundo arreglo
-    ["Master", "cual es mi mayor grado académico?"],
-    ["Université d'Orléans", "Donde estudie?"],
-];
-/*
-// mostrando los indices en los arreglos en la consola, sólo como ejemplo
-console.log(arregloTotal[0]);
-console.log(arregloTotal[0][0]);
-console.log(arregloTotal[0][1]);
-console.log(arregloTotal[1]);
-console.log(arregloTotal[1][0]);
-console.log(arregloTotal[2]);
-console.log(arregloTotal[2][0]);
-*/
-/*console.log("Usando FOR");
-for (let i=0; i<arregloTotal.length;i++){
-    console.log(arregloTotal[i][1]);
-    const respuesta = prompt(arregloTotal[i][1]);
-}
-*/
-//...
